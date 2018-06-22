d8773dd430b97e53589615f009468149e268ee7e
Aylin DeBruyne <adebruyne88@gmail.com>
2018-06-22 10:13:30 -0400

    add dummy data to folder

---
+++ dummydata.sql
@@ -0,0 +1,7 @@
+insert into Todos
+	(title, isDone)
+values
+    ('Find the one ring to rule them all', false),
+	('Feed Gollum', false),
+	('Talk to Elves', true),
+	('Hang out with Treebeard', true);
