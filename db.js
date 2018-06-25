3d2070d8ec57aa2921c31f9f51faf0f8cb7de14f
Aylin DeBruyne <adebruyne88@gmail.com>
2018-06-25 15:24:39 -0400

    ceate an edit option

--- db.js
+++ db.js
@@ -94,6 +94,7 @@ module.exports = {
     searchByTitle, 
     deleteById,
     setFinished,
+    setTitle,
     add
 
     
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const Todo = require('./db');
 
@@ -7,7 +9,8 @@ const expressHbs = require('express-handlebars');
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-
+const static = express.static;
+app.use(static('public'));
 
 
 
@@ -26,14 +29,62 @@ app.get('/', (req,res) => {
         .catch((error) => {console.log(error);});
 });
 
+
+app.get('/new', (req, res) =>{
+    res.render('todo-create-page')
+})
+
+app.post('/new', (req,res) => {
+    console.log(req.body);
+    // res.send('hey you submitted the form');
+    Todo.add(req.body.title)
+      .then((data) => {
+        //   console.log(data);
+        //   res.send(data);
+        res.redirect(`/${data.id}`);
+      })
+})
+
+app.get('/:id/edit',(req,res)=>{
+    Todo.getOne(req.params.id)
+        .then((data) => {
+            // res.send("edit this shiz");
+            res.render('todo-edit-page', data)
+        })
+    .catch((error) => {console.log(error);});
+})
+
+app.post('/:id/edit', (req,res) =>{
+    console.log(req.body);
+    var id = req.params.id;
+    var newTitle = req.body.title;
+
+    Todo.setTitle(id, newTitle)
+        .then((data) => {
+            console.log(data)
+            res.redirect(`/`)
+            
+
+        })
+        .catch((error) => {console.log(error);});
+})
+
+
+
+
 app.get('/:id' , (req,res) => {
     Todo.getOne(req.params.id)
         .then((data) => {
             console.log(data);
-            res.send(data);
+            // res.send(data);
+            res.render('todo-edit-page', data)
      })
         .catch((error) => {console.log(error);});
 })
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Your server is running!');
